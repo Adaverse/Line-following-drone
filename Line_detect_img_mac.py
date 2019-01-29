f1a3e84376ecf5d2bab48f6635c8034288b1ff22
Manik Mittal <45773405+thecalmguy@users.noreply.github.com>
2019-01-29 19:41:52 +0530

    Update and rename Line_detect_img.py to Line_detect_img_mac.py

--- Line_detect_img_mac.py
+++ Line_detect_img_mac.py
@@ -2,20 +2,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def slope(x1, y1, x2, y2):
-    m = (y2-y1)/(x2-x1)
-    return m
-
 img = cv2.imread('sample.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (9, 9), 0)
 
 #thresholding
-thresholded = cv2.threshold(gray,
-                                100,
-                                255,
-                                cv2.THRESH_BINARY)[1]
+thresholded = cv2.threshold(gray,100,255,cv2.THRESH_BINARY)
 #reversing the image
 thresholded = 255 - thresholded
 
